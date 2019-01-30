7f601207b166799c17f0d2ebae420ea54451a361
amanda-gomes <amacrisgomes@hotmail.com>
2019-01-30 16:50:26 -0200

    readme

--- README.md
+++ README.md
@@ -14,8 +14,6 @@ primeiros tipos de criptografias conhecidos na história. É um tipo de cifra po
 substituição, em que uma letra no texto original é substituída por outra,
 seguindo um número fixo para essa subtituição.
 
-![caeser-cipher](https://upload.wikimedia.org/wikipedia/commons/thumb/2/2b/Caesar3.svg/2000px-Caesar3.svg.png)
-
 O imperador Júlio César usou essa cifra para enviar ordens aos seus generais no
 campo de batalha. Essa é uma das técnicas mais simples e mais usadas para
 cifrar mensagens.
@@ -25,17 +23,7 @@ Por exemplo se usarmos o deslocamento de 3:
 * Alfabeto sem cifrar: A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
 * Alfabeto com cifra:  D E F G H I J K L M N O P Q R S T U V W X Y Z A B C
 
-Atualmente todas as cifras de substituição alfabética simples, são descifradas
-com facilidade e não oferecem muita segurança na comunição, mas a cifra de
-César muitas vezes pode fazer parte de um sistema mais complexo de
-criptografia, como a cifra de Vigenère, e tem aplicação no sistema ROT13.
-
-## Fluxograma
-
-![Fluxograma](src/img/diagrama-cipher.jpeg)
-
 ## Checklist
-
 ### Parte Obrigatória
 
 * [ ] `README.md` com explicações e desenho do projeto: No `README.md` descreva o seu processo de desenvolvimento utilizando os diagramas de bloco e como definiu os usuários quando estava desenvolvendo
@@ -52,10 +40,9 @@ seu projeto.
 * [ ] Implementa `cipher.decode`.
 * [ ] Criar testes unitários.
 
-### Parte Opcional: "Hacker edition"
+## Fluxograma
 
-* [ ] Permite usar um `offset` (_deslocamento_) negativo
-* [ ] Cifra/descifra _outros_ caracteres (espaços, pontuação, `ã`, `á`, ...)
+![Fluxograma](src/img/diagrama-cipher.jpeg)
 
 ### Scripts / Arquivos
 
--- src/cipher.js
+++ src/cipher.js
@@ -2,11 +2,11 @@ document.getElementById("cifrar").addEventListener("click", code);
 document.getElementById("decifrar").addEventListener("click", decode);
 document.getElementById("limpar").addEventListener("click", limpar);
 
-function cifrar (texto,offset){
+function cifrar (string,offset){
   let ntexto = "";
   let num;
-  for (let i = 0; i < texto.length; i++) {
-    num = texto.charCodeAt([i]);
+  for (let i = 0; i < string.length; i++) {
+    num = string.charCodeAt([i]);
     if (num >= 65 && num <= 90){
         num = ((num - 65 + offset) % 26) + 65;
         ntexto = ntexto.concat(String.fromCharCode(num));
@@ -23,24 +23,24 @@ function cifrar (texto,offset){
 }
 
 function code(){
-  const texto = document.getElementById("texto").value;
+  const string = document.getElementById("texto").value;
   const offset = parseInt(document.getElementById("offset").value);
-  const result = cifrar(texto,offset);
+  const result = cifrar(string,offset);
    
   document.getElementById("result").innerHTML = 
   ("<h3 id='titulo2'>Mensagem cifrada:</h3>"+
   "<textarea rows=5 cols=70>"+result+"</textarea><br>");
 }
 
-function decifrar (texto,offset){
+function decifrar (string,offset){
   let ntexto = "";
   let num;
-  for (let i = 0; i < texto.length; i++) {
-    num = texto.charCodeAt([i]);
+  for (let i = 0; i < string.length; i++) {
+    num = string.charCodeAt([i]);
     if (num >= 65 && num <= 90) {
         num = ((num - 65 - offset) % 26) + 65;
         if (num < 65){
-          num = ((texto.charCodeAt([i]) - 65 - offset) % 26) + 91;
+          num = ((string.charCodeAt([i]) - 65 - offset) % 26) + 91;
           ntexto = ntexto.concat(String.fromCharCode(num));
         } 
         else {
@@ -50,7 +50,7 @@ function decifrar (texto,offset){
     else if (num >= 97 && num <= 122){
         num = ((num - 97 - offset) % 26) + 97;
         if (num < 97){
-          num = ((texto.charCodeAt([i]) - 97 - offset) % 26) + 123;
+          num = ((string.charCodeAt([i]) - 97 - offset) % 26) + 123;
           ntexto = ntexto.concat(String.fromCharCode(num));
         } 
         else {
@@ -65,9 +65,9 @@ function decifrar (texto,offset){
 }
 
 function decode() {
-  const texto = document.getElementById("texto").value;
+  const string = document.getElementById("texto").value;
   const offset = parseInt(document.getElementById("offset").value);
-  const result = decifrar(texto,offset);
+  const result = decifrar(string,offset);
   
   document.getElementById("result").innerHTML = 
   ("<h3 id='titulo2'>Mensagem decifrada:</h3>"+
